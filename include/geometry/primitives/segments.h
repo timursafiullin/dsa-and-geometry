0e3dfa0d69736ea8546ac4902a4f8b914ae51033
Timur Safiullin <safiullintb@yandex.ru>
2026-08-07 10:27:44 +0500

    feat(geometry): add segments 2d, 3d implementations

---
+++ include/geometry/primitives/segments.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "geometry/primitives/vectors.h"
+#include "geometry/primitives/points.h"
+
+namespace dsa::geometry
+{
+
+    class Segment2
+    {
+    public:
+        Segment2(const Point2& start, const Point2& end);
+
+        const Point2& start() const;
+        const Point2& end() const;
+
+        vector2d direction() const;
+        double length() const;
+        Point2 pointAt(double t) const;
+
+    private:
+        Point2 start_;
+        Point2 end_;
+    };
+
+    class Segment3
+    {
+    public:
+        Segment3(const Point3& start, const Point3& end);
+
+        const Point3& start() const;
+        const Point3& end() const;
+
+        vector3d direction() const;
+        double length() const;
+        Point3 pointAt(double t) const;
+
+    private:
+        Point3 start_;
+        Point3 end_;
+    };
+
+}
